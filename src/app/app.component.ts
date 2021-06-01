518e748a60452788cfd55864cc6081691c848bd2
Giulio Giovanella <giulio.giovanella@berkan.com.br>
2021-06-01 11:17:55 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-proto';
+
+  funcionario:any = {
+    "nome": "gatuno",
+    "profissao": "Arquiteto de Software"
+  }
+  pdf = new jsPDF();
+  
+  imprimirCracha() {
+    console.log("gatuno imprime cracha");
+    
+    const element: HTMLElement | null = document.getElementById("cracha");
+    html2canvas(element as HTMLElement).then(canvas => {
+      const html = canvas.toDataURL("image/png");
+      
+      const imgWidth = 208;
+      const pageHeight = 295;
+      const imgHeight = canvas.height * imgWidth / canvas.height;
+      const position = 0;
+      //this.pdf.addImage(html, 'PNG', 0, position, imgWidth, imgHeight);
+      const pdf2 = new jsPDF();
+      pdf2.addImage(html, 'PNG', 0, position, 100, 100);
+
+      pdf2.save("a.pdf");
+    });
+    //this.pdf.save("cracha.pdf");
+
+    /*
+    doc.text("Hello world!", 20, 10);
+    doc.save("a4.pdf");
+    */
+  }
+
 }
