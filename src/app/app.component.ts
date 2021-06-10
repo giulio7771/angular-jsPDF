300a2d755fabb58626348e8945f8b59e75aa3725
eduardo valois franz <dudufranz13@gmail.com>
2021-06-10 12:05:04 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,71 @@
 import { Component } from '@angular/core';
-import { jsPDF } from "jspdf";
-import html2canvas from "html2canvas";
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-proto';
 
-  funcionario:any = {
-    "nome": "gatuno",
-    "profissao": "Arquiteto de Software"
-  }
+  funcionario: any = {
+    nome: 'gatuno',
+    profissao: 'Arquiteto de Software',
+  };
   pdf = new jsPDF();
-  
 
   imprimirCracha() {
-    console.log("imprimir cracha");
+    console.log('imprimir cracha');
     const scale = 2.2;
-    console.log("imprimindo");
-    var img = new Image()
-    img.src = "../assets/cracha.png";
+    console.log('imprimindo');
+    var img = new Image();
+    img.src = '../assets/cracha.png';
     const pdf = new jsPDF();
-    pdf.addImage(img, 'png', 0, 0, (54 * scale), (85.6 * scale));
+    pdf.addImage(img, 'png', 0, 0, 54 * scale, 85.6 * scale);
     pdf.setFontSize(10);
-    pdf.text("Gatuno da Silva", 18, 103);
-    pdf.text("Nr-18", 68, 75);
+    pdf.text('Gatuno da Silva', 18, 103);
+    pdf.text('Nr-18', 68, 75);
     pdf.setFontSize(9);
-    pdf.text("12/02/2022", 82, 75);
-    pdf.save("cracha.pdf");
+    pdf.text('12/02/2022', 82, 75);
+    pdf.save('cracha.pdf');
   }
 
   imprimirCracha7() {
-    var img = new Image()
-    img.src = "../assets/card-paint2.png";
+    var img = new Image();
+    img.src = '../assets/card-paint2.png';
     this.pdf.addImage(img, 'png', 20, 20, 200, 130);
-    this.pdf.text("gatuno", 22, 120);
+    this.pdf.text('gatuno', 22, 120);
     this.pdf.save();
-
   }
-
 
   imprimirCracha3() {
     const pdf = new jsPDF();
-    var img = new Image()
-    img.src = "../assets/card-paint.png";
-    pdf.addImage(img, 'png', 10, 78, 12, 15)
+    var img = new Image();
+    img.src = '../assets/card-paint.png';
+    pdf.addImage(img, 'png', 10, 78, 12, 15);
     pdf.save();
-
   }
 
   imprimirCracha2() {
-    console.log("gatuno imprime cracha");
-    
-    const element: HTMLElement | null = document.getElementById("cracha");
-    html2canvas(element as HTMLElement).then(canvas => {
-      const html = canvas.toDataURL("image/png");
-      
+    console.log('gatuno imprime cracha');
+
+    const element: HTMLElement | null =
+      document.getElementById('crachaContainer');
+    html2canvas(element as HTMLElement).then((canvas) => {
+      const html = canvas.toDataURL('image/png');
+
       const imgWidth = 208;
-      const pageHeight = 295;
-      const imgHeight = canvas.height * imgWidth / canvas.height;
+      const pageHeight = window.innerHeight;
+      const imgHeight = window.innerWidth;
       const position = 0;
       //this.pdf.addImage(html, 'PNG', 0, position, imgWidth, imgHeight);
       const pdf2 = new jsPDF();
-      pdf2.addImage(html, 'PNG', 0, position, 100, 100);
+      //pdf2.addHTML()
+      pdf2.addImage(html, 'PNG', 0, position, 20, 20);
 
-      pdf2.save("a.pdf");
+      pdf2.save('a.pdf');
     });
     //this.pdf.save("cracha.pdf");
 
@@ -76,5 +74,4 @@ export class AppComponent {
     doc.save("a4.pdf");
     */
   }
-
 }
